8ecabed84d88ba2c3c3c0acecac6b35d0ed2a766
leobabetto <147708841+leobabetto@users.noreply.github.com>
2023-11-10 19:26:01 +0100

    first commit

---
+++ mainTest.js
@@ -0,0 +1,48 @@
+// Esercizio 1
+const analizzaTesto = (`CamelCase dappertutto!`);
+() => {
+    const originale = [analizzaTesto];
+    const minuscolo = analizzaTesto.toLowerCase();
+    const senzaSpazi = analizzaTesto.strim():
+    const numeroCaratteri = analizzaTesto.length();
+    const contieneJS = analizzaTesto.includes(`JS`,`JavaScript`)
+    invertita => {
+        for(let str of analizzaTesto)
+        invertita += str
+    }
+};
+
+
+
+//Esercizio 2 
+const = riempiArray = () => {
+
+    let valMin = 1;
+    let valMax = 31;
+    let riempiArray = Math.floor( Math.random()valMin , valMax);
+    const = numeriPari []
+    const = numeriDispari []
+    
+}
+const = contaPariDispari = (riempiArray) => {
+
+    for(let i = 0; i < riempiArray.length; i++)
+    const num = riempiArray[i];
+    const riporto = num % 2;
+    if (riporto === 0){      
+        num.push(numeriPari)
+    }else{
+        num.push(numeriDispari)
+
+    }
+}
+
+
+
+//<------------------------------------------------------------------------------------------------------------------>
+// Esercizio 3
+window.addEventListener(`click`, () =>{
+    const input = document.querySelector(`input[type="testo utente"]`);
+
+})
+
